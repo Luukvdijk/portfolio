4f55a48b508233be2aaa26433d532a6b942deab6
Luuk van Dijk <luukvandijk2003@gmail.com>
2023-05-30 16:00:53 +0200

    about page

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -4,7 +4,22 @@ import styles from "./About.module.css";
 export default function About() {
   return (
     <div className={styles.mainContainer}>
-      <div>My about page</div>
+      <div className={styles.contentContainer}>
+        <div className={styles.left}>
+          <h1>
+            Ik ben Luuk, een <br /> web developer
+          </h1>
+          <h2>Front-end Developer</h2>
+        </div>
+        <div className={styles.right}>
+          <p>
+            Ik ben een ICT student met de focus op web development. <br />
+            <br /> Ik ben een probleem oplosser en heb een oog voor detail. Ik
+            ben geïnteresseerd in de frontend wereld en ik werk graag aan
+            ambitieuze projecten met interessante mensen.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
